8a6de08349075dfe7a804032b2b96c9c919b8eb3
metkm <metinkorkmaz417@gmail.com>
2024-11-28 16:23:45 +0300

    update busstop struct

--- src/models/bus.rs
+++ src/models/bus.rs
@@ -3,10 +3,16 @@ use serde::{Deserialize, Serialize};
 use crate::models::Coordinates;
 
 #[derive(Debug, Serialize, Deserialize)]
+pub struct BusStopPoint {
+    pub x: f64,
+    pub y: f64
+}
+
+#[derive(Debug, Serialize, Deserialize)]
 pub struct BusStop {
     pub stop_code: i32,
     pub stop_name: String,
-    pub coordinate: String,
+    pub coordinate: BusStopPoint,
     pub province: String,
     pub direction: String,
     pub smart: String,
