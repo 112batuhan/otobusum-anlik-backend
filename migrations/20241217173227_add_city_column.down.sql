d52c377dd909a850aa26d0b93882cd7814c2cd1b
metkm <metinkorkmaz417@gmail.com>
2024-12-19 15:58:56 +0300

    ugh

--- migrations/20241217173227_add_city_column.down.sql
+++ migrations/20241217173227_add_city_column.down.sql
@@ -4,8 +4,7 @@ ALTER TABLE route_paths DROP city;
 ALTER TABLE stops DROP city;
 ALTER TABLE line_stops DROP city;
 
-ALTER TABLE lines DROP INDEX id_code_index_lines;
-ALTER TABLE routes DROP INDEX id_code_index_routes;
-ALTER TABLE route_paths DROP INDEX id_code_index_route_paths;
-ALTER TABLE stops DROP INDEX id_code_index_stops;
-ALTER TABLE line_stops DROP INDEX id_code_index_line_stops;
+-- ALTER TABLE lines DROP INDEX id_code_index_lines;
+-- ALTER TABLE routes DROP INDEX id_code_index_routes;
+-- ALTER TABLE route_paths DROP INDEX id_code_index_route_paths;
+-- ALTER TABLE stops DROP INDEX id_code_index_stops;
