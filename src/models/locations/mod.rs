16b3221a3e05d526ef476384e07472fbfab3d468
metkm <metinkorkmaz417@gmail.com>
2025-01-01 14:04:59 +0300

    fix typo

--- src/models/locations/mod.rs
+++ src/models/locations/mod.rs
@@ -13,7 +13,7 @@ pub struct BusLocation {
     pub lng: f64,
     pub lat: f64,
     pub route_code: String,
-    pub closes_stop_code: Option<u32>,
+    pub closest_stop_code: Option<u32>,
 }
 
 impl From<BusLocationIst> for BusLocation {
@@ -23,7 +23,7 @@ impl From<BusLocationIst> for BusLocation {
             lat: value.lat,
             lng: value.lng,
             route_code: value.route_code,
-            closes_stop_code: Some(value.closest_stop_code),
+            closest_stop_code: Some(value.closest_stop_code),
         }
     }
 }
@@ -37,7 +37,7 @@ impl BusLocation {
             lat: value.x_coord, // izmir returns coordinates reversed for some reason
             lng: value.y_coord,
             route_code: format!("{line_code}_{}_D0", dir),
-            closes_stop_code: None,
+            closest_stop_code: None,
         }
     }
 }
